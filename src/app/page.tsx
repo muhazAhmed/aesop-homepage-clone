230b675716d4189b6078ccc4345da310d15d46c7
muhazAhmed <muhazvla313@gmail.com>
2025-01-04 11:30:19 +0530

    added festive section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-// import FestiveSection from "@/components/FestiveSection";
+import FestiveSection from "@/components/FestiveSection";
 import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <div className="relative w-full h-full">
       <Hero />
-      {/* <FestiveSection /> */}
+      <FestiveSection />
     </div>
   );
 }
